4dacbc28a4afb637bab46be180537b1b79e759d7
leonlee021 <leonlee021@gmail.com>
2022-07-20 15:26:16 +0900

    css background layouts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './styles/styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div className="app__header">
+        <h1>Twittagram</h1>
+      </div>
+      <div class="app__feed">
+        <div className="app__feed__left">
+          <h2>feed__left</h2>
+        </div>
+        <div className="app__feed__right">
+          <h2>feed__right</h2>
+        </div>
+      </div>
+      {/* Header */}
+      {/* Post */}
+    </div >
   );
 }
 
